a7197cb83740eeb0cc17558dc1832a4e29c6a6ca
CHAUVEAU Jeremy <CHAUVEAU@dev.groupe.adg>
2018-12-13 13:45:05 +0100

    Ajoutez des fichiers projet.

---
+++ Quetzal/Quetzal/PageItem.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Quetzal
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageItem : ContentPage
+	{
+		public PageItem ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("titre", "message affiché", "OK");
+        }
+    }
+}
